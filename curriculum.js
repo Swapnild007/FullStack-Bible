465251cdbcbe29d93c30ac17c6e4dabbe683eff7
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-17 17:03:44 +0530

    Remove FullStack Bible files

--- curriculum.js
+++
@@ -1,7 +0,0 @@
-document.write('<script src="curriculum-data.js"><\/script>');
-document.write('<script src="lesson-content.js"><\/script>');
-document.write('<script src="deep-content.js"><\/script>');
-document.write('<script src="theme-loader.js"><\/script>');
-document.write('<script src="app-enhancements.js"><\/script>');
-document.write('<script src="mobile-layout.js"><\/script>');
-document.write('<script src="mobile-fix-v2.js"><\/script>');
