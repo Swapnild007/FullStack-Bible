ab92d1d83e714ec41597e4bb52e808680bf2d4f6
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-17 16:49:36 +0530

    Load responsive mobile reading layout

--- curriculum.js
+++ curriculum.js
@@ -3,3 +3,4 @@ document.write('<script src="lesson-content.js"><\/script>');
 document.write('<script src="deep-content.js"><\/script>');
 document.write('<script src="theme-loader.js"><\/script>');
 document.write('<script src="app-enhancements.js"><\/script>');
+document.write('<script src="mobile-layout.js"><\/script>');
