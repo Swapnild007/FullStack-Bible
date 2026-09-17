063abdbd187d9fe6987acd434daa1342803cecb1
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-17 15:39:29 +0530

    Wire deep learning engine into curriculum loader

--- curriculum.js
+++ curriculum.js
@@ -2,3 +2,4 @@ document.write('<script src="curriculum-data.js"><\/script>');
 document.write('<script src="lesson-content.js"><\/script>');
 document.write('<script src="deep-content.js"><\/script>');
 document.write('<script src="theme-loader.js"><\/script>');
+document.write('<script src="app-enhancements.js"><\/script>');
