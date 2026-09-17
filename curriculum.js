fbee89aa282da68e920dc378e64cd741142f3f6e
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-17 15:34:37 +0530

    Load interactive learning system

--- curriculum.js
+++ curriculum.js
@@ -1,3 +1,4 @@
-document.write('<script src="curriculum-data.js"><\/script>');
-document.write('<script src="lesson-content.js"><\/script>');
-document.write('<script src="theme-loader.js"><\/script>');
+document.write('<script src="curriculum-data.js?v=3"><\/script>');
+document.write('<script src="lesson-content.js?v=3"><\/script>');
+document.write('<script src="theme-loader.js?v=3"><\/script>');
+document.write('<script src="app-enhancements.js?v=1"><\/script>');
