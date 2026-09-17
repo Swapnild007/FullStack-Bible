5dc29d97d90874d71a50ef72f24a8f29178220a0
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-17 15:28:50 +0530

    build: load deep lesson content before workspace

--- curriculum.js
+++ curriculum.js
@@ -1,2 +1,3 @@
 document.write('<script src="curriculum-data.js"><\/script>');
+document.write('<script src="lesson-content.js"><\/script>');
 document.write('<script src="theme-loader.js"><\/script>');
